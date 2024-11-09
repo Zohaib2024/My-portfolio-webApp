bcebef9955da62c23112850b7ff6ba50ebe6ee64
Zohaib2024 <162030191+Zohaib2024@users.noreply.github.com>
2024-11-09 18:55:30 +0500

    Update Social.tsx

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -5,24 +5,23 @@ import SocialCard from './SocialCard'
 const SocialData =[
     {
         companyName:"Facebook",
-        desc:" This is good This is good This is good This is good This is good This is good This is good This is good",
+        desc:" The layout is responsive and works well on mobile, but there are some minor styling issues on smaller screens. Could we adjust the padding and font sizes to improve readability on mobile?",
         img:"/todoapp.jpg",
-        name:'Mark zukerberg',
-        designation: "Facebook",
+        name:'Liam',
+        designation: "UX Designer",
     },
     {
         companyName:"Google",
-        desc:" This is good This is good This is good This is good This is good This is good This is good This is good",
-        img:"/todoapp.jpg",
-        name:'Mark zukerberg',
-        designation: "Facebook",
+        desc:" The page load times are impressive! Next.js is definitely helping with performance, but maybe we could optimize images a bit more for an even faster experience.",
+        name:'Emma',
+        designation: "Front-End Developer",
     },
     {
         companyName:"Instagram",
-        desc:" This is good This is good This is good This is good This is good This is good This is good This is good",
+        desc:" The routing feels smooth, and I love the use of server-side rendering for dynamic content. Adding some animations between page transitions might make the experience feel even more seamless!",
         img:"/todoapp.jpg",
-        name:'Mark zukerberg',
-        designation: "Facebook",
+        name:'Noah',
+        designation: "QA Engineer",
     }
 ]
 const Social = () => {
