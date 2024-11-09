bffcb16d733b0be20ea2528febd93a1286f60139
Zohaib2024 <162030191+Zohaib2024@users.noreply.github.com>
2024-11-09 18:57:08 +0500

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,8 +7,7 @@ const Skills = () => {
             <div className='ml-16 ' data-aos="zoom-in-up">
                 <h2 className='text-4xl md:text-5xl '>Technologies work with</h2>
                 <p className='text-gray-500 pt-2'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Iure a asperiores libero, accusamus aliquid dolor illo quas modi earum praesentium expedita esse minus ullam fugit!
+                   You can practice your skills by building real-world applications. For example, you can work on creating a streaming service clone or an image-sharing platform. These projects will allow you to tackle complex problems like authentication, routing, and data management 
                 </p>
             </div>
             <div className='grid grid-cols-2 text-accent text-3xl sm:text-4xl sm:ml-16 md:ml-1 lg:ml-16  place-items-center'>
