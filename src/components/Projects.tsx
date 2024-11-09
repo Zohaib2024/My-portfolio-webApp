425d9767cf7262067fcf068d16b695b715a5da84
Zohaib2024 <162030191+Zohaib2024@users.noreply.github.com>
2024-11-09 18:47:45 +0500

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,8 @@
-import { title } from 'process'
+
 import React from 'react'
 import Heading from './Heading'
-
 import Card from './Card'
-import { link } from 'fs'
+
 
 const Projects = () => {
     const data = [
